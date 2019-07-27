6e3470753f064dbc50e7f635138e80e071a3424c
Rick Tijink <ricktijink@hotmail.com>
2019-07-27 21:17:58 +0200

    Updates

---
+++ precache-manifest.a8705ba1386469c0b389abc837771d04.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5786cc03d52acbc583383c878d47cbd9",
+    "url": "/cafe-avada/index.html"
+  },
+  {
+    "revision": "c95cfb00dc51225dd2cb",
+    "url": "/cafe-avada/static/css/main.5ceed284.chunk.css"
+  },
+  {
+    "revision": "e6c20bc94cd08a1b3def",
+    "url": "/cafe-avada/static/js/2.dbe27321.chunk.js"
+  },
+  {
+    "revision": "c95cfb00dc51225dd2cb",
+    "url": "/cafe-avada/static/js/main.4ccf801b.chunk.js"
+  },
+  {
+    "revision": "75d781abd69333fcaab4",
+    "url": "/cafe-avada/static/js/runtime~main.e189cdd6.js"
+  },
+  {
+    "revision": "2baf499ec262a3d7801d5898875fc8d0",
+    "url": "/cafe-avada/static/media/article-img-1.2baf499e.png"
+  },
+  {
+    "revision": "23958a575111ebc015ce42cf5620b714",
+    "url": "/cafe-avada/static/media/article-img-2.23958a57.png"
+  },
+  {
+    "revision": "dfbfb8827a28961f26972368d4189bea",
+    "url": "/cafe-avada/static/media/article-img-3.dfbfb882.png"
+  },
+  {
+    "revision": "19001f1c43b0e61a347e7c46bb3bd34b",
+    "url": "/cafe-avada/static/media/blog-four-thumb.19001f1c.jpg"
+  },
+  {
+    "revision": "fa329e0dab55e5af782b61e3b5ec594f",
+    "url": "/cafe-avada/static/media/blog-one-thumb.fa329e0d.jpg"
+  },
+  {
+    "revision": "defe593b397499e381c3feb02a5b7a20",
+    "url": "/cafe-avada/static/media/blog-three-thumb.defe593b.jpg"
+  },
+  {
+    "revision": "5402548a881d37e1faec363ea84a6e22",
+    "url": "/cafe-avada/static/media/blog-two-thumb.5402548a.jpg"
+  },
+  {
+    "revision": "5a8e43cc07fd954ab6d1cb54a050e94c",
+    "url": "/cafe-avada/static/media/drinks.5a8e43cc.jpg"
+  },
+  {
+    "revision": "086a3dddccfd4271ebe999600c70104e",
+    "url": "/cafe-avada/static/media/footer-logo.086a3ddd.png"
+  },
+  {
+    "revision": "bcb38c247adda13b698c5bf2eeec9d30",
+    "url": "/cafe-avada/static/media/hero-aboutus.bcb38c24.png"
+  },
+  {
+    "revision": "b366961a8c97e469fa2ba42a323c58f2",
+    "url": "/cafe-avada/static/media/hero-banner-logo.b366961a.png"
+  },
+  {
+    "revision": "eb2ed7bf3a52bbc713a5cafc1245dd09",
+    "url": "/cafe-avada/static/media/hero-banner.eb2ed7bf.jpg"
+  },
+  {
+    "revision": "e536b3ef1702120e03f061e0e0f57a15",
+    "url": "/cafe-avada/static/media/hero-foodsanddrinks.e536b3ef.png"
+  },
+  {
+    "revision": "0adefee475abfe34d9c2bb216c391dec",
+    "url": "/cafe-avada/static/media/hero-locations.0adefee4.jpg"
+  },
+  {
+    "revision": "c3da736e544c66263779d151d7f2aaff",
+    "url": "/cafe-avada/static/media/hero-ourstory.c3da736e.png"
+  },
+  {
+    "revision": "30fd5bac6985e5123b79ac5d383f503b",
+    "url": "/cafe-avada/static/media/img1.30fd5bac.jpg"
+  },
+  {
+    "revision": "1b00a581d55ce8bba60510f216f72992",
+    "url": "/cafe-avada/static/media/img2.1b00a581.jpg"
+  },
+  {
+    "revision": "294ec1ca6acbb3d387a58c868813f9ef",
+    "url": "/cafe-avada/static/media/img3.294ec1ca.png"
+  },
+  {
+    "revision": "2f511c22c016a35e759065ae122fb3a0",
+    "url": "/cafe-avada/static/media/main-logo.2f511c22.png"
+  },
+  {
+    "revision": "057c5c4ffa59503a5e94a754e0eb84cd",
+    "url": "/cafe-avada/static/media/recommended-dessert.057c5c4f.jpg"
+  },
+  {
+    "revision": "402e6cbcabde170b15d8ed7e8bbda830",
+    "url": "/cafe-avada/static/media/recommended-maincourse.402e6cbc.jpg"
+  },
+  {
+    "revision": "6f3eaf8e04972c8e4a9849963ba87e48",
+    "url": "/cafe-avada/static/media/recommended-starter.6f3eaf8e.jpg"
+  },
+  {
+    "revision": "144a952358b662371520ac41a48447c6",
+    "url": "/cafe-avada/static/media/reservations-bg.144a9523.png"
+  },
+  {
+    "revision": "4f8c2d9a9ea757891c2283ffec32a561",
+    "url": "/cafe-avada/static/media/small-item-1.4f8c2d9a.png"
+  },
+  {
+    "revision": "d8c07ba367b3dbff3cb4a8ec24c2076b",
+    "url": "/cafe-avada/static/media/small-item-2.d8c07ba3.png"
+  },
+  {
+    "revision": "c201e82ef8ae3b0bce6c6a7fd324f7d7",
+    "url": "/cafe-avada/static/media/small-item-3.c201e82e.png"
+  },
+  {
+    "revision": "9381da7d4c92428eebed74454cfe9359",
+    "url": "/cafe-avada/static/media/small-item-4.9381da7d.png"
+  }
+]);
